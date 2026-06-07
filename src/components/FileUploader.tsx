2def633158ce8537c1d09b65ed8a5c502bf40679
pramudya-yb <pramudyayusuf23@gmail.com>
2026-06-07 19:32:38 +0700

    Update FileUploader.tsx

--- src/components/FileUploader.tsx
+++ src/components/FileUploader.tsx
@@ -149,7 +149,6 @@ export default function FileUploader({ files, onFilesAdded, onFileRemove, onClea
             </>
           )}
         </div>
-      </div>
         <input
           ref={fileInputRef}
           type="file"
